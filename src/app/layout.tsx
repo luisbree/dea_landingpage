da81fd7fbf683286c2feaff48426b13d9477f454
Luis Bree <LuisBree@gmail.com>
2025-08-02 13:10:54 +0000

    no

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased bg-transparent">
         <div className="fixed inset-0 -z-10">
           <Image
             src="/fondo_dea.jpg"
