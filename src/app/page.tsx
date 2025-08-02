f34085ffc1b8a604a36baa09f975ee0fbe4c8a21
Luis Bree <LuisBree@gmail.com>
2025-08-02 13:16:26 +0000

    Joya. Ahora, pongamos la imagen como único elemento.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
-  return null;
+  return (
+    <main className="relative h-screen w-screen">
+      <Image
+        src="/fondo_dea.jpg"
+        alt="Fondo de pantalla"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+      />
+    </main>
+  );
 }
